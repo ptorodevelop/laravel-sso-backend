bac22ff1f66124232c70d8b05502573b5c0537ca
ptorodevelop <ptorodevelop@gmail.com>
2025-10-10 10:09:46 -0500

    fix: generated services v2

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,13 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:api')->get('process/restricted', [ProcessController::class, 'index']);
 });
 
-// // V2
-// Route::prefix('v2')->group(function () {
-//     Route::post('auth/login', [AuthControllerV2::class, 'login']);
-//     Route::middleware('auth:api')->get('process/restricted', [ProcessControllerV2::class, 'index']);
-// });
+// V2
+Route::prefix('v2')->group(function () {
+    Route::post('auth/login', [AuthControllerV2::class, 'login']);
+    Route::middleware('auth:api')->get('process/restricted', [ProcessControllerV2::class, 'index']);
+});
 
-// // V3
+// V3
 // Route::prefix('v3')->group(function () {
 //     Route::post('auth/login', [AuthControllerV3::class, 'login']);
 //     Route::middleware('auth:api')->get('process/restricted', [ProcessControllerV3::class, 'index']);
